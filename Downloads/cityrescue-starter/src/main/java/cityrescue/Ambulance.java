0b6f0282d2b0375a6c57464880a71c8b78631e49
james-jackman <jj654@exeter.ac.uk>
2026-03-04 16:45:44 +0000

    comments

--- Downloads/cityrescue-starter/src/main/java/cityrescue/Ambulance.java
+++ Downloads/cityrescue-starter/src/main/java/cityrescue/Ambulance.java
@@ -3,16 +3,21 @@ package cityrescue;
 import cityrescue.enums.UnitType;
 import cityrescue.enums.IncidentType;
 
+//ambulance unit for medical incidents
 public class Ambulance extends Unit {
+   
+    //set up ambulance with station and starting position
     public Ambulance(int unitId, int homeStationId, int startX, int startY) {
         super(unitId, UnitType.AMBULANCE, homeStationId, startX, startY);
     }
 
+    //checks that unit can deal with incident
     @Override
     public boolean canHandleIncidentType(IncidentType incidentType) {
         return incidentType == IncidentType.MEDICAL;
     }
 
+    //returns ticks taken to resolve
     @Override
     public int getTicksToResolve() { return 2; }
 }
