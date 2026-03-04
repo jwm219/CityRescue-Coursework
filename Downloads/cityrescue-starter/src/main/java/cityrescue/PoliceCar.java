17d39a2cb236ded9e5045945d416b0a831993273
james-jackman <jj654@exeter.ac.uk>
2026-03-04 16:54:52 +0000

    comments

--- Downloads/cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
+++ Downloads/cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
@@ -3,16 +3,21 @@ package cityrescue;
 import cityrescue.enums.UnitType;
 import cityrescue.enums.IncidentType;
 
+//police unit for crime emergencies
 public class PoliceCar extends Unit {
+
+    //sets police unit with station and start position
     public PoliceCar(int unitId, int homeStationId, int startX, int startY) {
         super(unitId, UnitType.POLICE_CAR, homeStationId, startX, startY);
     }
 
+    //checks that unit can deal with incident
     @Override
     public boolean canHandleIncidentType(IncidentType incidentType) {
         return incidentType == IncidentType.CRIME;
     }
 
+    //returns ticks taken to resolve
     @Override
     public int getTicksToResolve() { return 3; }
 }
