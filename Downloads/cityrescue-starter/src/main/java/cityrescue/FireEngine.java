c3011f70bb7d636bd601f6b6ef7d8f84ef677ecd
james-jackman <jj654@exeter.ac.uk>
2026-03-04 16:50:42 +0000

    comments

--- Downloads/cityrescue-starter/src/main/java/cityrescue/FireEngine.java
+++ Downloads/cityrescue-starter/src/main/java/cityrescue/FireEngine.java
@@ -3,16 +3,21 @@ package cityrescue;
 import cityrescue.enums.UnitType;
 import cityrescue.enums.IncidentType;
 
+//fire engine unit for fire incidents
 public class FireEngine extends Unit {
+
+    //set up fire engine with station and start position
     public FireEngine(int unitId, int homeStationId, int startX, int startY) {
         super(unitId, UnitType.FIRE_ENGINE, homeStationId, startX, startY);
     }
 
+    //checks that unit can deal with incident
     @Override
     public boolean canHandleIncidentType(IncidentType incidentType) {
         return incidentType == IncidentType.FIRE;
     }
 
+    //returns ticks taken to resolve
     @Override
     public int getTicksToResolve() { return 4; }
 }
